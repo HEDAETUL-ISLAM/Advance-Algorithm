e7f47878e525be6be306dfc3b9b012793e225166
Hedaetul Islam <bulbulhedaytulislam@gmail.com>
2019-09-28 02:32:47 +0600

    simple reflex agent vacuum cleaning problem two

---
+++ Vacuum Cleaning Agent/Python/problemTwo.py
@@ -0,0 +1,72 @@
+import random
+
+class Environment(object):
+    def __init__(self):
+        # set location and conditions 0 for clean 1 for dirty
+        self.locationCondition = {'A':'0', 'B': '0'}
+
+        self.locationCondition['A'] = random.randint(0,1)
+        self.locationCondition['B'] = random.randint(0,1)
+
+class SimpleReflesVaccumeAgent(Environment):
+    def __init__(self, Environment):
+        print (Environment.locationCondition)
+        score = 0
+        vacumeLocation = random.randint(0,1) #move vacume randomly 
+        
+        if vacumeLocation == 0:
+            print ("vaccume in randomly placed in location A")
+            if Environment.locationCondition['A'] == 1:
+                print ("location A is dirty")
+                Environment.locationCondition['A'] = 0
+                score += 1
+                print( "location A has been cleaned")
+                
+                
+                if Environment.locationCondition['B'] == 1:
+                    print ("location B is dirty")
+                    print ("moving to position B")
+                    score -= 1
+                    Environment.locationCondition['B'] = 0
+                    score += 1
+                    print ("location B has been cleaned")
+
+            else:
+                if Environment.locationCondition['B'] == 1:
+                    print("location B is dirty")
+                    score -= 1
+                    print("moving location B")
+                    Environment.locationCondition['B'] = 0
+                    score += 1
+                    print("Location B has been cleared")
+
+        elif vacumeLocation == 1:
+            print ("Vaccume randomly placed in location B")
+            if Environment.locationCondition['B'] == 1:
+                print("Location B is dirty")
+                Environment.locationCondition['B'] = 0
+                score += 1
+                print("Location B has cleaned")
+                
+                if Environment.locationCondition['A'] == 1:
+                    print ("location A is dirty")
+                    print("Moving location A")
+                    score -= 1
+                    Environment.locationCondition['A'] = 0
+                    score += 1
+                    print ("location A has been cleaned")
+
+            else:
+                if Environment.locationCondition['A'] == 1:
+                    print("Location is dirty")
+                    print("moveing to A")
+                    score -= 1
+                    Environment.locationCondition['A'] = 0
+                    score += 1
+                    print("Location A has been cleaned")
+        
+        print (Environment.locationCondition)
+        print("Performance MeasureL : " +str(score))
+
+theEnvironment = Environment()
+theVacume = SimpleReflesVaccumeAgent(theEnvironment)
